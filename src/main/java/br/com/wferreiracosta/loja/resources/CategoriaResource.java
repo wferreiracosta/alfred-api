60b235c89b68186212bedb7c616f2f89d8b7a641
Wesley Costa <wesley.costa.developer@gmail.com>
2019-07-21 02:31:44 -0300

    Corrigindo espacos

--- src/main/java/br/com/wferreiracosta/loja/resources/CategoriaResource.java
+++ src/main/java/br/com/wferreiracosta/loja/resources/CategoriaResource.java
@@ -1,7 +1,5 @@
 package br.com.wferreiracosta.loja.resources;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
