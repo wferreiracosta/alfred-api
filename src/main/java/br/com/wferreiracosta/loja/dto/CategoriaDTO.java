e11c8ff881684e2ce7a715a68580a7b233f896b5
Wesley Costa <wesley.costa.developer@gmail.com>
2019-07-31 09:50:10 -0300

    Validacao sintatica com bean validation

--- src/main/java/br/com/wferreiracosta/loja/dto/CategoriaDTO.java
+++ src/main/java/br/com/wferreiracosta/loja/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package br.com.wferreiracosta.loja.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.wferreiracosta.loja.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
--- src/main/java/br/com/wferreiracosta/loja/services/CategoriaService.java
+++ src/main/java/br/com/wferreiracosta/loja/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.wferreiracosta.loja.domain.Categoria;
+import br.com.wferreiracosta.loja.dto.CategoriaDTO;
 import br.com.wferreiracosta.loja.repositories.CategoriaRepository;
 import br.com.wferreiracosta.loja.services.exceptions.DataIntegrityException;
 import br.com.wferreiracosta.loja.services.exceptions.ObjectNotFoundException;
@@ -52,6 +53,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
