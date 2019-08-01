ac4a12ef83d7b5abc8564464eb60fdf1e01bdc55
Wesley Costa <wesley.costa.developer@gmail.com>
2019-08-01 15:54:31 -0300

    Implementado PUT, DELETE, GET para cliente

--- src/main/java/br/com/wferreiracosta/loja/domain/Cliente.java
+++ src/main/java/br/com/wferreiracosta/loja/domain/Cliente.java
@@ -54,6 +54,13 @@ public class Cliente implements Serializable {
 		this.tipo = tipo.getCod();
 	}
 
+	public Cliente(Integer id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
 	public Integer getId() {
 		return id;
 	}
--- src/main/java/br/com/wferreiracosta/loja/services/ClienteService.java
+++ src/main/java/br/com/wferreiracosta/loja/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.wferreiracosta.loja.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.wferreiracosta.loja.domain.Cliente;
+import br.com.wferreiracosta.loja.dto.ClienteDTO;
 import br.com.wferreiracosta.loja.repositories.ClienteRepository;
+import br.com.wferreiracosta.loja.services.exceptions.DataIntegrityException;
 import br.com.wferreiracosta.loja.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,6 +25,39 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = this.find(obj.getId());
+		this.updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		this.find(id);
+		try {
+			repo.deleteById(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page,Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail());
 	}
 
 }
