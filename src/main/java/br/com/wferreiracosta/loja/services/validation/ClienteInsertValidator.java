ee5b2b6ca65e0cdf960d0b16ef53332ada765796
Wesley Costa <wesley.costa.developer@gmail.com>
2019-08-03 16:48:29 -0300

    Validacao costumizada na insercao de cliente

--- src/main/java/br/com/wferreiracosta/loja/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/wferreiracosta/loja/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.wferreiracosta.loja.domain.Cliente;
 import br.com.wferreiracosta.loja.domain.enums.TipoCliente;
 import br.com.wferreiracosta.loja.dto.ClienteNewDTO;
+import br.com.wferreiracosta.loja.repositories.ClienteRepository;
 import br.com.wferreiracosta.loja.resources.exception.FieldMessage;
 import br.com.wferreiracosta.loja.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido!"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("email","O e-mail informado já existe!"));
 		}
 		
 		for (FieldMessage e : list) {
