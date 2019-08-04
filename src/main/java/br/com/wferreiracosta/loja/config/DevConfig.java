55d276d2fd4132007e1de6d58d9469944a7d9563
Wesley Costa <wesley.costa.developer@gmail.com>
2019-08-04 14:56:14 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/wferreiracosta/loja/config/DevConfig.java
@@ -0,0 +1,31 @@
+package br.com.wferreiracosta.loja.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.wferreiracosta.loja.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if(!strategy.equals("create")) {
+			return false;
+		}
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
