b6e62eee41ded6f41dd5bf4abdbc969cd53200d3
Wesley Costa <wesley.costa.developer@gmail.com>
2019-11-02 19:48:12 -0300

    Implementando autorizacao

--- src/main/java/br/com/wferreiracosta/loja/config/SecurityConfig.java
+++ src/main/java/br/com/wferreiracosta/loja/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.wferreiracosta.loja.security.JWTAuthenticationFilter;
+import br.com.wferreiracosta.loja.security.JWTAuthorizationFilter;
 import br.com.wferreiracosta.loja.security.JWTUtil;
 
 @Configuration
@@ -42,14 +43,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
 
 		http.cors().and().csrf().disable();
-		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
-				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
--- src/main/java/br/com/wferreiracosta/loja/services/DBService.java
+++ src/main/java/br/com/wferreiracosta/loja/services/DBService.java
@@ -114,20 +114,16 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA, pe.encode("123"));
-		Cliente cli2 = new Cliente(null, "Wesley Costa", "wferreiracosta@outlook.com", "36378912377", TipoCliente.PESSOA_FISICA, pe.encode("123"));
-		cli2.addPerfil(Perfil.ADMIN);
+		Cliente cli1 = new Cliente(null, "Wesley Costa", "wferreiracosta@outlook.com", "36378912377", TipoCliente.PESSOA_FISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-		cli2.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
-		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli2, c2);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1));
-		cli2.getEnderecos().addAll(Arrays.asList(e2));
 
-		clienteRepository.saveAll(Arrays.asList(cli1,cli2));
+		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
