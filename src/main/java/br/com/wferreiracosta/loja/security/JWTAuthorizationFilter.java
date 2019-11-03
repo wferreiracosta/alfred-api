57287447e5a729a7eeaae1e8356893197622d3b3
Wesley Costa <wesley.costa.developer@gmail.com>
2019-11-03 17:27:59 -0300

    Configurando a passagem de autorização pelo Header

--- src/main/java/br/com/wferreiracosta/loja/security/JWTAuthorizationFilter.java
+++ src/main/java/br/com/wferreiracosta/loja/security/JWTAuthorizationFilter.java
@@ -31,7 +31,7 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
                                     HttpServletResponse response,
                                     FilterChain chain) throws IOException, ServletException {
 
-		String header = request.getParameter("Authorization");
+		String header = request.getHeader("Authorization");
 		if (header != null && header.startsWith("Bearer ")) {
 			UsernamePasswordAuthenticationToken auth = getAuthentication(header.substring(7));
 			if (auth != null) {
