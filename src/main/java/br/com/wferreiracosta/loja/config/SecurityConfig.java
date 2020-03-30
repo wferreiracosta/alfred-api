bcdc97d02af9546f13cd09cd3585f044c03c779c
Wesley Costa <24726458+wferreiracosta@users.noreply.github.com>
2020-03-30 01:22:39 -0300

    Update SecurityConfig.java

--- src/main/java/br/com/wferreiracosta/loja/config/SecurityConfig.java
+++ src/main/java/br/com/wferreiracosta/loja/config/SecurityConfig.java
@@ -44,11 +44,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
 			"/categorias/**",
-			"/categorias/**"
+			"/categorias/**",
+			"/pedidos/**"
 	};
 
 	private static final String[] PUBLIC_MATCHERS_POST = {
-			"/clientes/**"
+			"/clientes/**",
+			"/pedidos/**"
 	};
 
 	@Override
