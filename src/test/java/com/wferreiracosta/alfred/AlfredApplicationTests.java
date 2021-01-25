1ce7017d9e9a942c647a43223e88edfa8f986f45
Wesley Costa <wesferrcosta@gmail.com>
2021-01-25 15:12:57 -0300

    Criado projeto

---
+++ src/test/java/com/wferreiracosta/alfred/AlfredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wferreiracosta.alfred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
