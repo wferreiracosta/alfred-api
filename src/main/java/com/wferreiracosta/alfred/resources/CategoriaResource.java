ec69d93f501ad99ed9dc7a4a6db4436e7c1cf294
Wesley Costa <wesley.costa@b2wdigital.com>
2021-01-27 20:16:38 -0300

    Testando requisição

---
+++ src/main/java/com/wferreiracosta/alfred/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.wferreiracosta.alfred.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @GetMapping
+    public String listar(){
+        return "OK";
+    }
+
+}
