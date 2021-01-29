d4f1f32c0034d244fce8b5b76c66c759913778f0
Wesley Costa <wesley.costa@b2wdigital.com>
2021-01-28 22:37:26 -0300

    Retirado metodo de teste do resource

--- src/main/java/com/wferreiracosta/alfred/resources/CategoriaResource.java
+++ src/main/java/com/wferreiracosta/alfred/resources/CategoriaResource.java
@@ -1,37 +1,10 @@
 package com.wferreiracosta.alfred.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.wferreiracosta.alfred.domain.Categoria;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-
-    @GetMapping
-    public List<Categoria> listar() {
-        Categoria cat1 = Categoria
-            .builder()
-            .id(1)
-            .nome("Informática")
-            .build();
-
-        Categoria cat2 = Categoria
-            .builder()
-            .id(2)
-            .nome("Escritorio")
-            .build();
-
-        List<Categoria> categorias = new ArrayList<>();
-        categorias.add(cat1);
-        categorias.add(cat2);
-
-        return categorias;
-    }
 
 }
