7bd36296246aefc86aa5ca29a42494d1d75de5ef
Wesley Costa <wesferrcosta@gmail.com>
2021-02-03 17:27:27 -0300

    Add ActiveProfiles no AlfredApplicationTests

--- src/test/java/com/wferreiracosta/alfred/AlfredApplicationTests.java
+++ src/test/java/com/wferreiracosta/alfred/AlfredApplicationTests.java
@@ -2,8 +2,10 @@ package com.wferreiracosta.alfred;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AlfredApplicationTests {
 
 	@Test
