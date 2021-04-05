14106a0221986646e7b23e1b0b96643e8e9aa7b4
Wesley Costa <wesferrcosta@gmail.com>
2021-04-04 21:04:49 -0300

    Feat(Categoria): Retornar todas as categorias
    Criado service que retornar todas as categorias

--- src/main/java/com/wferreiracosta/alfred/service/CategoriaService.java
+++ src/main/java/com/wferreiracosta/alfred/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.wferreiracosta.alfred.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.wferreiracosta.alfred.domain.Categoria;
@@ -7,5 +8,6 @@ import com.wferreiracosta.alfred.domain.Categoria;
 public interface CategoriaService {
 
 	Optional<Categoria> findById(Integer id);
+	List<Categoria> findAll();
 
 }
