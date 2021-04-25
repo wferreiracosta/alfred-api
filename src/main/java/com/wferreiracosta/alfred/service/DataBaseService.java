60ea45d48e996145c6a6902698c72296c38f0ce0
Wesley Costa <wesferrcosta@gmail.com>
2021-04-24 23:21:18 -0300

    Criado interface do DataBaseService

--- src/main/java/com/wferreiracosta/alfred/service/DataBaseService.java
+++ src/main/java/com/wferreiracosta/alfred/service/DataBaseService.java
@@ -11,34 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DataBaseService {
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private ProdutoRepository produtoRepository;
+public interface DataBaseService {
 
-	public void instantiateTestDatabase() {
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
-		Categoria cat4 = new Categoria(null, "Eletrônicos");
+	public void instantiateTestDatabase();
 
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Toalha", 80.00);
-		Produto p4 = new Produto(null, "Calculadora", 50.00);
-
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2));
-		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
-		cat3.getProdutos().addAll(Arrays.asList(p3));
-		cat4.getProdutos().addAll(Arrays.asList(p4));
-
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat3));
-		p4.getCategorias().addAll(Arrays.asList(cat4));
-
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4));
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
-	}
 }
