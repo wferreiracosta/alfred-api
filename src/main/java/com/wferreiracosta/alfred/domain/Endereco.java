22a916f2b4dd38504b30eea0f03fb35662762a5c
Wesley Costa <wesferrcosta@gmail.com>
2021-05-13 19:32:16 -0300

    feat: Incluido atributo de cidade no Endereco

--- src/main/java/com/wferreiracosta/alfred/domain/Endereco.java
+++ src/main/java/com/wferreiracosta/alfred/domain/Endereco.java
@@ -33,4 +33,8 @@ public class Endereco implements Serializable {
     @JoinColumn(name="cliente_id")
     private Cliente cliente;
 
+    @ManyToOne
+    @JoinColumn(name="cidade_id")
+    private Cidade cidade;
+
 }
