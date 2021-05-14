f5e8b2ba8545bc54840b15463977b14e93a456a6
Wesley Costa <wesferrcosta@gmail.com>
2021-05-13 21:12:43 -0300

    Tirado Builder

--- src/main/java/com/wferreiracosta/alfred/domain/Cliente.java
+++ src/main/java/com/wferreiracosta/alfred/domain/Cliente.java
@@ -1,7 +1,9 @@
 package com.wferreiracosta.alfred.domain;
 
 import com.wferreiracosta.alfred.domain.enums.TipoCliente;
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -9,7 +11,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Builder
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
