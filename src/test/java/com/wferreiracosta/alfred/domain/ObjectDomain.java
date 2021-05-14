eecec6cc4f8d460179cb16e195203a53fa0669bc
Wesley Costa <wesferrcosta@gmail.com>
2021-05-13 23:31:15 -0300

    Apagado classas de criação de objetos domain

---
+++ src/test/java/com/wferreiracosta/alfred/domain/ObjectDomain.java
@@ -0,0 +1,44 @@
+package com.wferreiracosta.alfred.domain;
+
+import com.wferreiracosta.alfred.domain.enums.TipoCliente;
+
+public class ObjectDomain {
+
+    public static Categoria criarCategoriaSemId() {
+        return Categoria.builder()
+                .nome("Categoria Teste")
+                .build();
+    }
+
+    public static Categoria criarCategoriaComId() {
+        return Categoria.builder()
+                .id(1)
+                .nome("Categoria Teste")
+                .build();
+    }
+
+    public static Cliente criarClienteSemId() {
+        return Cliente.builder()
+                .nome("Pedro")
+                .cpfOuCnpj("18683776018")
+                .email("pedro@silva.com")
+                .tipo(TipoCliente.PESSOAFISICA.getId())
+                .build();
+    }
+
+    public static Produto criarProdutoSemId() {
+        return Produto.builder()
+                .nome("Computador")
+                .preco(10.00)
+                .build();
+    }
+
+    public static Produto criarProdutoComId() {
+        return Produto.builder()
+                .id(1)
+                .nome("Computador")
+                .preco(10.00)
+                .build();
+    }
+
+}
