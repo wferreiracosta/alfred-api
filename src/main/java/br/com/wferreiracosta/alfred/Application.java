cacc6964fb5669413cb3dcd2895c561821ccabc5
Wesley Costa <wesferrcosta@gmail.com>
2025-11-22 20:58:57 -0300

    Ajustado de LojaApplication para Application

--- src/main/java/br/com/wferreiracosta/alfred/Application.java
+++ src/main/java/br/com/wferreiracosta/alfred/Application.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LojaApplication implements CommandLineRunner {
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LojaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Override
